140fd23658dcd142a2205559e56a3ed9500eb3a6
Dorian Duez <dodu.duez@gmail.com>
2025-12-04 16:56:22 +0100

    Maj  Navbar

--- projet-finance/src/components/Navbar.jsx
+++ projet-finance/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 export default function NavBar() {
     return (
-        <nav className="navbar">
-            <h1>Projet Finances</h1>
+        <nav className="bg-gray-800 shadow-neumorphic sticky top-0 z-40">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1>Projet Finances</h1>
+            </div>
         </nav>
     )
 }
